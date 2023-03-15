3f341f5203e9e821402cc3694ee2fc34b1eacd15
Aaron Gonzales <agonz243@ucsc.edu>
2023-03-15 12:46:59 -0700

    Incorporated beginning text into story scene

--- tracery.js
+++ tracery.js
@@ -6,18 +6,19 @@ let sentences = [
     "THE END"];
 let data;
 
-let beginningText = [`One fine, interstellar morning, #hero# waltzed into the control room of Galactic Enterprise T-Ship 0345.`, 
-`#hero# was part of the cleaning crew for the ships Time-inator 9000 the galaxy's first working time machine!`, 
-`As usual, #heroThey# went about their usual routine, completing boring tasks like sweeping up #objects#, #objects#, 
-and #objects# from the Time-inator's platform`, `Suddenly, #hero# tripped over some #objects# and crashed into the Time-inator's controls!`, 
-`With a loud BUZZZ and a few CLINKS and CLANGS, the machine turned on with a bright flash!`]
+let beginningText = [`One fine, interstellar morning, #hero# waltzed into the control room of Galactic Enterprise T-Ship 0345.| 
+#hero# was part of the cleaning crew for the ships Time-inator 9000 the galaxy's first working time machine!| 
+As usual, #heroThey# went about #heroTheir# usual routine, completing boring tasks like sweeping up #objects#, #objects#, 
+and #objects# from the Time-inator's platform|Suddenly, #hero# tripped over some #objects# and crashed into the Time-inator's controls!| 
+With a loud BUZZZ and a few CLINKS and CLANGS, the machine turned on with a bright flash!`]
 
 let words =
 {
     story: {
         "origin": ["#[#setPronouns#][#setSetting#][hero:#name#]story#"],
-        "story": ["#data#|#introduction#"],
+        "story": ["#data#|#beginning#|#introduction#"],
         "data": ['{"setting": "#setting#"}'],
+        "beginning": [],
         "introduction": ["After a rough tumble, #hero# opened #heroTheir# eyes and was shocked to find a #setting# before #heroThem#."],
         "middle-intro": ["As #hero# shook off #heroTheir# confusion, a steady rumble caught #heroTheir# attention.", 
             "While #hero# was still finding #heroTheir# bearings #heroThey# sensed something and quickly looked up at their surroundings.",
@@ -29,7 +30,7 @@ let words =
             "[heroThey:he][heroThem:him][heroTheir:his][heroTheirs:his]"],
         "setSetting": ["[setting:prehistoric land]", "[setting:futuristic city]", "[setting:victorian castle]"],
 
-        "objects": ["dinosaur bones, medieval armor, robot parts"]
+        "objects": ["dinosaur bones", "medieval armor", "robot parts"]
     }
 };
 
@@ -37,6 +38,9 @@ let grammar;
 
 
 function loadGrammar() {
+    // Set beginning text 
+    words.story.beginning = beginningText;
+
     grammar = tracery.createGrammar(words["story"]);
     let raw = grammar.flatten('#origin#');
 
