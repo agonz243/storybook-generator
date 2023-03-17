4d56380453e39264cbb41922ce3f0c86af817b7a
grammb <108758537+grammb@users.noreply.github.com>
2023-03-16 22:30:01 -0700

    updated tracery, needs finish ending

--- tracery.js
+++ tracery.js
@@ -27,18 +27,28 @@ let words =
             "While #hero# was still finding #heroTheir# bearings #heroThey# sensed something and quickly looked up at #heroTheir# surroundings.",
             "#heroThey# froze as #heroThey# noticed a towering shadow looming behind #heroThem# and slowly turned..."],
         "middle-plot": [ "#heroThey# stood in awe as #heroThey# gazed at a pack of fluffy T-rexes bounding after their prey",
-            "#hero# was stunned by the sight of a massive battle between #enemy.s#, #enemy.s#, and #enemy.s#!"],
+            "#hero# was stunned by the sight of a massive battle between #enemy.s#, #enemy.s#, and #enemy.s#!",
+            " The crash of cymbals and swords greeted our #hero# as a crowd of #enemy.s# met with a horde of #enemy.s#.",
+            "A blinking terminal cast a neon glow over the #setting#, overrun with #enemy.s#?!", //want to use same setting as above here, how do?
+            " A gaggle of #types# #enemy.s# zoomed past #heroThem#, in hurried pursuit of a lone #enemy#",
+            "As if an everyday occurance, a band of #enemy.s# playing #instrument.s# accompanied a choir of #enemy.s"],
         "intermission": [],
+        //ask aaron how to reuse the variable for time machine name and location
+        "end-plot":["Through the <blank> and under the <blank> #hero# spotted a familiar gleam... The #machine#! There was just one problem..."], 
+        "end-plot2":[],//want to have another "problem" here, can mirror above format for middle plot, or somehow keep middle plot selection and write individual solns for them?
         "name": ["Aaron,Anika,Julie,Justin,Graham"],
         "setPronouns": ["[heroThey:they][heroThem:them][heroTheir:their][heroTheirs:theirs]",
             "[heroThey:she][heroThem:her][heroTheir:her][heroTheirs:hers]",
             "[heroThey:he][heroThem:him][heroTheir:his][heroTheirs:his]"],
         "setSetting": ["[setting:prehistoric land]", "[setting:futuristic city]", "[setting:victorian castle]"],
 
-        "objects": ["dinosaur bones", "medieval armor", "robot parts", "ancient swords", "futuristic gizmos", "dino eggs"],
+        "objects": ["dinosaur bones", "medieval armor", "robot parts", "ancient swords", "futuristic gizmos", "dino eggs", "flux capacitors", "forbidden tomes", "thagomizer fossils", "cow tools"],
         "noise": ["BUZZ", "BANG", "POP", "POW", "SKADOOSH", "WHIRR", "PVVVT", "ZRRT"],
         "machine": ["Time-Inator 9000", "Delorian Mk 2", "Time Warp Whirligig", "Timey Wimey XR100", "Quantum Leapfrog"],
-        "enemy": ["velociraptor", "pterodactyl", "knight", "wizard", "archer", "killer robot", "space ship"]
+        "enemy": ["velociraptor", "pterodactyl", "knight", "wizard", "archer", "killer robot", "space ship", "neanderthal", ""],
+        "types":["sorcerer", "cyborg", "apprentice", "brigand", "bard", "prehistoric", "psychic"],
+        "thingies":["club", "orb", "staff", "rock", "spear", "lasgun", "fang"],
+        "instruments":["lute", "flute", "synthesizer", "harp", "drum"]
     }
 };
 
